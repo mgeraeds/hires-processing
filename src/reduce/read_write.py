8514c6ad0919891750714444000f86a05e06e96e
meggeraeds <m.e.g.geraeds@tudelft.nl>
2024-04-24 13:49:05 +0200

    Updated input validation with validation function from src/reduce/validate

--- src/reduce/read_write.py
+++ src/reduce/read_write.py
@@ -7,11 +7,12 @@ import xarray as xr
 import glob
 import numpy as np
 import warnings
+from src.reduce.validate_input import validate_input
 
 
 if __name__ == '__main__':
 
-    # > Set up argument parsing
+    # Set up argument parsing
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--file', type=str, required=True)
     parser.add_argument('-o', '--out_file', type=str)  # type=dir_path)
@@ -19,7 +20,7 @@ if __name__ == '__main__':
     parser.add_argument('-kv', '--keep_variables', nargs='+', type=str, required=True)
     args = parser.parse_args()
 
-    # > Get arguments
+    # Get arguments
     file_nc = args.file
     out_file = args.out_file
     batch_cores = args.batch_cores
@@ -42,28 +43,7 @@ if __name__ == '__main__':
 
     ## 2. Input tests
     #----------------------------------------------------------------------------------------
-    # 2.1. Loop through all of the files and write when there is something wrong with the file
-    file_list = glob.glob(file_nc)
-
-    # 2.2 Make a list for corrupted files
-    corrupted = []
-
-    for file in file_list:
-
-        file_name = file.split('/')[-1]
-        
-        try:
-            test = xr.open_dataset(file)
-        except:
-            warnings.warn(f'The file {file_name} seems to be corrupted. Check your inputs.', UserWarning)
-            corrupted.append(file)
-
-    # 2.3 Check if there are any corrupted files in the listed files
-    if len(corrupted) > 0:
-        print('There are corrupted files. These will be taken out of the file list for the next tests.')
-        
-    # 2.4 Get the entire input list and remove the corrupted files
-    input_files = [f for f in glob.glob(file_nc) if not f in corrupted]
+    input_files = validate_input(file_nc)
 
     ## 3. Open the partitioned dataset with xarray
     #----------------------------------------------------------------------------------------
@@ -79,7 +59,6 @@ if __name__ == '__main__':
     # 4.2 Select only the relevant variables
     # > {gridname}_node_z is necessary to keep the  nodal dimension.
     ds = ds[keep_variables]
-    # ds = ds[[f'{gridname}_node_z', f'{gridname}_sa1', f'{gridname}_vol1', f'{gridname}_tem1', f'{gridname}_vicwwu']] # f'{gridname}_au',
 
     # 4.3 Define counter for file initialization and later appending
     i = 0
@@ -87,14 +66,14 @@ if __name__ == '__main__':
     # 4.4 Loop through variables and save as netCDF file in loop
     for v in keep_variables:
 
-        # > Select the time
+        # Select the variable
         print(f'Starting selection of variable {v}...')
         vds = ds[v]
         print(f'Loaded the dataset at variable {v}.')
 
         if i == 0:
-            # > For t = 0 in the range of timesteps, check if there's a file
-            # > already present. If it is, remove it.
+            # First check if there's a file already present. 
+            # If it is, remove it.
             if os.path.isfile(out_file):
                 os.remove(out_file)
 
@@ -103,14 +82,16 @@ if __name__ == '__main__':
             vds.ugrid.to_netcdf(out_file, mode='w', compute=True)
 
         else:
-            # Use dask.delayed to write file to disk
+            # Use dask.delayed to write file to disk in append mode
             print(f'Writing to file {out_file} in append mode...')
             print(f"Outputfile: {out_file}")
             vds.ugrid.to_netcdf(out_file, mode='a', compute=True)
 
+        # Close the file to make room in memory
         vds.close()
 
-        # > Update counter
+        # Update counter
         i += 1
-
+    
+    # Close the client
     client.close()
