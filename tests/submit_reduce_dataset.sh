4013cd5406964f7c713a1cec653a4ae9111b2323
meggeraeds <m.e.g.geraeds@tudelft.nl>
2024-05-14 17:30:54 +0200

    Updated with new file to reduce

--- tests/submit_reduce_dataset.sh
+++ tests/submit_reduce_dataset.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes 1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=192
-#SBATCH --job-name=post
+#SBATCH --job-name=postD02AU
 #SBATCH --partition=genoa
 #SBATCH --time=6:00:00
 #SBATCH --mail-type=BEGIN,END,ERROR
@@ -28,7 +28,7 @@ conda activate dfm_proc_env # > Change this to environment with all of the requi
 
 # Initialize filenames & script names
 run_name=D02
-run_id=AU
+run_id=AP
 filebase=RMD_dflowfm_${run_name}${run_id} #RMM_dflowfm_2022_jul21_aug7_sm
 pythonfile=/home/mgeraeds/Repositories/hires-processing/src/reduce/read_write.py
 outfile=/home/mgeraeds/Repositories/hires-processing/tests/${run_name}${run_id}_red.nc # Change to output location with filename
@@ -41,4 +41,4 @@ echo $TIMENOW
 max_mem=336 #$((336/SLURM_CPUS_PER_TASK))
 
 which python3
-python3 $pythonfile -f /projects/0/einf1300/saltis-wp3-1/C_Work/05_RMD_2019/computations/${run_name}/${run_name}${run_id}/${filebase}_000*_map.nc -o $outfile -m $max_mem -n 8 -c time=100 -k 'mesh2d_sa1' 'mesh2d_node_z' 'mesh2d_ucx' 'mesh2d_ucy' 'mesh2d_ucz' 'mesh2d_vol1' 'mesh2d_vicwwu' 'mesh2d_tem1' 'mesh2d_face_nodes' 'mesh2d_edge_nodes' 'mesh2d' -e '{"mesh2d_face_nodes": {"dtype": "float32"}, "mesh2d_edge_nodes": {"dtype": "float64"}, "mesh2d_nNodes": {"dtype":"int32"}, "mesh2d_nFaces": {"dtype":"int32"}, "mesh2d_nEdges": {"dtype":"int32"}, "mesh2d_flowelem_zcc": {"dtype":"float32"}, "mesh2d_flowelem_zw": {"dtype":"float32"}, "mesh2d_face_x": {"dtype":"float32"}, "mesh2d_face_y": {"dtype":"float32"}, "mesh2d_edge_x": {"dtype":"float32"}, "mesh2d_edge_y": {"dtype":"float32"}, "mesh2d_sa1": {"dtype":"float32"}, "mesh2d_node_x": {"dtype":"float32"}, "mesh2d_node_y": {"dtype":"float32"}, "mesh2d_node_z": {"dtype":"float32"}, "mesh2d_ucx": {"dtype":"float32"}, "mesh2d_ucy": {"dtype":"float32"}, "mesh2d_ucz": {"dtype":"float32"}, "mesh2d_vol1": {"dtype":"float32"}, "mesh2d_vicwwu": {"dtype":"float32"}, "mesh2d_tem1":{"dtype":"float32"}}' > ~/%x.%j.pro
+python3 $pythonfile -f /projects/0/einf1300/saltis-wp3-1/C_Work/05_RMD_2019/computations/${run_name}/${run_name}${run_id}/${filebase}_00*_map.nc -o $outfile -m $max_mem -n 8 -c time=100 -k 'mesh2d' 'mesh2d_sa1' 'mesh2d_s1' 'mesh2d_node_z' 'mesh2d_ucx' 'mesh2d_ucy' 'mesh2d_ucz' 'mesh2d_flowelem_zw' 'mesh2d_vol1' 'mesh2d_au' 'mesh2d_rho' 'mesh2d_vicwwu' 'mesh2d_tem1' 'mesh2d_face_nodes' 'mesh2d_edge_nodes' -e '{"mesh2d_face_nodes": {"dtype": "float32"}, "mesh2d_edge_nodes": {"dtype": "float64"}, "mesh2d_nNodes": {"dtype":"int32"}, "mesh2d_nFaces": {"dtype":"int32"}, "mesh2d_nEdges": {"dtype":"int32"}, "mesh2d_flowelem_zcc": {"dtype":"float32"}, "mesh2d_flowelem_zw": {"dtype":"float32"}, "mesh2d_face_x": {"dtype":"float32"}, "mesh2d_face_y": {"dtype":"float32"}, "mesh2d_edge_x": {"dtype":"float32"}, "mesh2d_edge_y": {"dtype":"float32"}, "mesh2d_sa1": {"dtype":"float32"}, "mesh2d_node_x": {"dtype":"float32"}, "mesh2d_node_y": {"dtype":"float32"}, "mesh2d_node_z": {"dtype":"float32"}, "mesh2d_ucx": {"dtype":"float32"}, "mesh2d_ucy": {"dtype":"float32"}, "mesh2d_ucz": {"dtype":"float32"}, "mesh2d_vol1": {"dtype":"float32"}, "mesh2d_vicwwu": {"dtype":"float32"}, "mesh2d_tem1":{"dtype":"float32"}}' > ~/%x.%j.pro
