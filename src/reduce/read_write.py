8938822935b16169da7322902d24f6e958449f8d
meggeraeds <m.e.g.geraeds@tudelft.nl>
2024-05-14 10:36:20 +0200

    Added function structure

--- src/reduce/read_write.py
+++ src/reduce/read_write.py
@@ -39,11 +39,31 @@ def parse_slice(time_slice):
                 raise ValueError('Input of time_slice argument gives incorrect slice.')
         
     return time_slice
+def print_and_log(text):
+    print(text)
+    with open("logfile.txt", "a") as logfile:
+        logfile.write(text+"\n")
 
-def reduce_dataset(files, out_file, keep_vars, chunks, encoding, time_slice):#, validate_input=False):
+def reduce_dataset(files, out_file, keep_vars, **kwargs): 
     """ 
     TODO: docstring formatted for Sphinx
     """
+    ## 1. Get kwargs
+    if 'chunks' in kwargs:
+        chunks = kwargs['chunks']
+    if 'encoding' in kwargs:
+        encoding = kwargs['encoding']
+    # if 'validate_input' in kwargs:
+    #     validate_input = kwargs['validate_input']
+    #     if validate_input == None:
+    #         validate_input = False
+    #     else: 
+    #         validate_input = True
+    if 'time_slice' in kwargs:
+        time_slice = kwargs['time_slice']
+
+    print_and_log(f'Used keyword arguments are: {kwargs}')
+
     ## 2. Input tests
     #----------------------------------------------------------------------------------------
     # if validate_input: 
@@ -54,7 +74,7 @@ def reduce_dataset(files, out_file, keep_vars, chunks, encoding, time_slice):#, 
 
     ## 3. Open the partitioned dataset with xarray
     #----------------------------------------------------------------------------------------
-    print('Loading large dataset...')
+    print_and_log('Loading large dataset...')
     try:
         ds = dfmt.open_partitioned_dataset(input_files, chunks=chunks)
     except:
@@ -63,7 +83,7 @@ def reduce_dataset(files, out_file, keep_vars, chunks, encoding, time_slice):#, 
         except:
             raise Exception('Could not load files. Check your inputs and/or regex.')
         
-    print('Large dataset loaded.')
+    print_and_log('Large dataset loaded.')
 
     ## 4. Get variables and subset of complete dataset
     #----------------------------------------------------------------------------------------
@@ -84,16 +104,19 @@ def reduce_dataset(files, out_file, keep_vars, chunks, encoding, time_slice):#, 
     else:
         ds = ds[keep_vars]
 
-    # > Select the required time period
+    # > Select the required time period (if indicated)
     try:
-        tds = ds.isel(time_slice)
+        tds = ds.isel(time=time_slice)
     except:
-        tds = ds.sel(time_slice)
+        try:
+            tds = ds.sel(time_slice=time_slice)
+        except:
+            tds = ds
 
     for v in keep_vars:
 
         # > Select the time
-        print(f'Starting selection of variable {v}...')
+        print_and_log(f'Starting selection of variable {v}...')
         vds = tds[v] 
 
         # > If there's encoding specified, make a subset for the variables in that dataset only
@@ -107,34 +130,36 @@ def reduce_dataset(files, out_file, keep_vars, chunks, encoding, time_slice):#, 
                 os.remove(out_file)
 
             # Use dask.delayed to write file to disk
-            print('Writing file to disk...')
+            print_and_log('Writing file to disk...')
             vds.to_netcdf(out_file, mode='w', compute=True, encoding=encoding_sub)
 
         else:
             # Use dask.delayed to write file to disk
-            print('Writing file to disk in append mode...')
+            print_and_log('Writing file to disk in append mode...')
             vds.to_netcdf(out_file, mode='a', compute=True, encoding=encoding_sub)
 
         # writing_task.compute()
         vds.close()
 
-        print(f"Outputfile: {out_file}")
+        print_and_log(f"Outputfile: {out_file}")
         # > Update counter
         i += 1
-        
-if __name__ == '__main__':
 
+
+if __name__ == '__main__':
+    
     # Set up argument parsing
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--files', type=str, required=True, help="file regex of the files to open")
+    parser.add_argument('-f', '--files', nargs='+', type=str, required=True, help="file regex of the files to open")
     parser.add_argument('-o', '--out_file', type=str, help="the name of the output file")  # type=dir_path)
-    parser.add_argument('-p', '--n_processes', type=str, required=True, help="the amount of processes to spawn with the dask client")
+    parser.add_argument('-n', '--n_processes', type=str, required=True, help="the amount of processes to spawn with the dask client")
     parser.add_argument('-m', '--max_mem', type=int, required=True, help="the maximum memory that can be used by the dask client")
     parser.add_argument('-k', '--keep_vars', nargs='+', type=str, required=True, help="the variables to keep in the final output file")
     parser.add_argument('-c', '--chunks', nargs='*', action=ParseKwargs, help="the desired chunks, given as <dimension>=n_chunks")
-    parser.add_argument('-v', '--validate_input', default=False, type=bool, help="boolean defining whether to validate the input regex or not")
+    # parser.add_argument('-v', '--validate_input', default=False, type=bool, help="boolean defining whether to validate the input regex or not")
     parser.add_argument('-e', '--encoding', default={}, type=json.loads, help="dictionary defining the encoding passed to the to_netcdf call")
     parser.add_argument('-t', '--time_slice', type=parse_slice, nargs='+', default='all', help="slice or time (int or specific time) to select for the output file")
+
     args = parser.parse_args()
 
     # Get arguments
@@ -142,9 +167,9 @@ if __name__ == '__main__':
     out_file = args.out_file
     max_mem = args.max_mem
     n_processes = args.n_processes
-    keep_vars = args.keep_variables
+    keep_vars = args.keep_vars
     chunks = args.chunks
-    validate_input = args.validate_input
+    # validate_input = args.validate_input
     encoding = args.encoding
     time_slice = args.time_slice
 
@@ -152,15 +177,15 @@ if __name__ == '__main__':
     #----------------------------------------------------------------------------------------
     mem_lim = str(int(np.floor(max_mem))) + 'GB'
 
-    print(f"Allocated memory to the dask client is {mem_lim}.")
+    print_and_log(f"Allocated memory to the dask client is {mem_lim}.")
 
-    print('Starting client...')
-    client = Client(n_workers=n_processes, memory_limit=mem_lim)
-    client.amm.start()  # automatic memory management
+    print_and_log('Starting client...')
+    client = Client(n_workers=int(n_processes), memory_limit=mem_lim)
+    client.amm.start() # automatic memory management
 
     # 2. Apply reduce_dataset function
     #----------------------------------------------------------------------------------------
-    reduce_dataset(files=files, out_file=out_file, keep_vars=keep_vars, chunk=chunks, encoding=encoding, time_slice=time_slice)#, validate_input=validate_input)
+    reduce_dataset(files=files, out_file=out_file, keep_vars=keep_vars, chunks=chunks, encoding=encoding, time_slice=time_slice)
 
     # 3. Close the client
     #----------------------------------------------------------------------------------------
